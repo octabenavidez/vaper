f3ed423bb237753072ad5ceea8c29b5d347d85d7
octabenavidez <octabenavidezsarmiento@gmail.com>
2022-08-17 22:15:17 -0300

    ihpone

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-if (ereg("Mobile.*Safari",$_SERVER['HTTP_USER_AGENT'])) {
-    const sectionPrincipal = document.querySelector("#bgPrincipal")
-    const h1 = document.createElement("h1")
-    h1.innerText = "Hola"
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+    if (document.cookie.indexOf("iphone_redirect=false") == -1){
+        const sectionPrincipal = document.querySelector("#bgPrincipal")
+        const h1 = document.createElement("h1")
+        h1.innerText = "Hola"
+        
+        sectionPrincipal.append(h1);
+    } 
+ }
+
+
+// if (ereg("Mobile.*Safari",$_SERVER['HTTP_USER_AGENT'])) {
+//     const sectionPrincipal = document.querySelector("#bgPrincipal")
+//     const h1 = document.createElement("h1")
+//     h1.innerText = "Hola"
     
-    sectionPrincipal.append(h1)
-}
+//     sectionPrincipal.append(h1)
+// }
 
 // if (ereg("Mobile.*Safari",$_SERVER['HTTP_USER_AGENT'])) {
 //     const video = document.querySelector("#bgvid")
