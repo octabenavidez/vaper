59c517496cc4a1e85e8914fd8402e3efe3bcebcb
octabenavidez <octabenavidezsarmiento@gmail.com>
2022-08-17 22:09:42 -0300

    Cambios js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 if (ereg("Mobile.*Safari",$_SERVER['HTTP_USER_AGENT'])) {
-    const video = document.querySelector("#bgvid")
-    video.remove()
-
-    const section = document.querySelector("#sectionVideo") 
-    const img = document.createElement("img")
-    img.setAttribute("src", "pexels-tetyana-kovyrina-937980.jpg");
-
-    section.append(img)
+    const sectionPrincipal = document.querySelector("#bgPrincipal")
+    const h1 = document.createElement("h1")
+    h1.innerText = "Hola"
+    
+    sectionPrincipal.append(h1)
 }
+
+// if (ereg("Mobile.*Safari",$_SERVER['HTTP_USER_AGENT'])) {
+//     const video = document.querySelector("#bgvid")
+//     video.remove()
+
+//     const section = document.querySelector("#sectionVideo") 
+//     const img = document.createElement("img")
+//     img.setAttribute("src", "pexels-tetyana-kovyrina-937980.jpg");
+
+//     section.append(img)
+// }
