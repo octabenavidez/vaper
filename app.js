75261370d2d53bde1abac02e12b34b553b1cf007
octabenavidez <octabenavidezsarmiento@gmail.com>
2022-11-08 22:59:39 -0300

    Adelanto supremevape casifinal4

--- app.js
+++ app.js
@@ -1,26 +1,62 @@
-// Comprobacion de si el telefono es iphone
 
-if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
-    if (document.cookie.indexOf("iphone_redirect=false") == -1){
-        const sectionVideo = document.querySelector("#sectionVideo")
-        const video = document.querySelector("#bgvid")
-        video.remove();
+window.addEventListener('DOMContentLoaded', (event) => {
+  // Slider
+  let slider = document.querySelectorAll('.slider');
 
-        const img = document.createElement("img")
-        img.setAttribute("src", "pexels-tetyana-kovyrina-937980.jpg");
-        img.className = "video"
+  let i = 0;
+
+  function funcionSliders() {
+    slider.forEach(slider => {
+      let sliderSection = slider.children;
+      let sliderSectionLast = sliderSection[sliderSection.length - 1];
+    
+      let btnLeft = document.querySelectorAll('.slider__btn--left')[i];
+      let btnRight = document.querySelectorAll('.slider__btn--right')[i];
+    
+      ++i
+    
+      slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+    
+      function next() {
+        let sliderSectionFirst = slider.children[0];
         
-        sectionVideo.append(img);
-    } 
-
-    window.addEventListener('DOMContentLoaded', () => {
-      let slider = document.querySelectorAll('.slider');
-
-      let i = 0;
-
-      funcionSliders();
+        slider.style.marginLeft = "-200%";
+        slider.style.transition = "all 0.5s";
+    
+        setTimeout(() => {
+          slider.style.transition = "none";
+          slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+          slider.style.marginLeft = "-100%";
+        }, 500);
+      }
+    
+      function prev() {
+        let sliderSection = slider.children;
+        let sliderSectionLast = sliderSection[sliderSection.length - 1];
+        
+        slider.style.marginLeft = "0";
+        slider.style.transition = "all 0.5s";
+    
+        setTimeout(() => {
+          slider.style.transition = "none";
+          slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+          slider.style.marginLeft = "-100%";
+        }, 500);
+      }
+    
+      btnRight.addEventListener("click", function(){
+        next();
+      });
+      
+      btnLeft.addEventListener("click", function(){
+        prev();
+      });
+    
     });
-}
+  }
+
+  funcionSliders();
+});
 
 // Popup inicial
 
@@ -52,68 +88,6 @@ if (popup === null){
       })
   });
 } 
-
-// Slider
-
-let slider = document.querySelectorAll('.slider');
-
-let i = 0;
-
-function funcionSliders() {
-  slider.forEach(slider => {
-    let sliderSection = slider.children;
-    let sliderSectionLast = sliderSection[sliderSection.length - 1];
-  
-    let btnLeft = document.querySelectorAll('.slider__btn--left')[i];
-    let btnRight = document.querySelectorAll('.slider__btn--right')[i];
-  
-    ++i
-  
-    slider.insertAdjacentElement("afterbegin", sliderSectionLast);
-  
-    function next() {
-      let sliderSectionFirst = slider.children[0];
-      
-      slider.style.marginLeft = "-200%";
-      slider.style.transition = "all 0.5s";
-  
-      setTimeout(() => {
-        slider.style.transition = "none";
-        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
-        slider.style.marginLeft = "-100%";
-      }, 500);
-    }
-  
-    function prev() {
-      let sliderSection = slider.children;
-      let sliderSectionLast = sliderSection[sliderSection.length - 1];
-      
-      slider.style.marginLeft = "0";
-      slider.style.transition = "all 0.5s";
-  
-      setTimeout(() => {
-        slider.style.transition = "none";
-        slider.insertAdjacentElement("afterbegin", sliderSectionLast);
-        slider.style.marginLeft = "-100%";
-      }, 500);
-    }
-  
-    btnRight.addEventListener("click", function(){
-      next();
-    });
-    
-    btnLeft.addEventListener("click", function(){
-      prev();
-    });
-  
-  
-  
-  });
-}
-
-funcionSliders();
-
-
 
 // setInterval(() => {
 //   next();
